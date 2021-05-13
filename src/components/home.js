91daa96b5f9f6dddcbab630d3ce16a7fd0685282
Dewa <adewalade@gmail.com>
2021-05-13 20:41:32 +0100

    feat: add styling to home cards

--- src/components/home.js
+++ src/components/home.js
@@ -16,19 +16,29 @@ export default function Home () {
     let homes;
 
         homes = homesState.map((home) => {
-            return(
-                <div data-testid="home" key={home.id}>
-                    <div data-testid="home-title">{home.title}</div>
-                    <img data-testid="home-image" src={home.image} alt=""/>
-                    <div data-testid="home-location">{home.location}</div>
-                    <div data-testid="home-price">{home.price}</div>
+            return (
+              <div className="col-6 col-md-6 col-lg-4 col-xl-3 m-b-3" key={home.id}>
+                <div data-testid="home" className="card w-100 home-card">
+                  <img data-testid="home-image" src={home.image} alt="home img" className="card-img-top" height="200" />
+                  <div className="card-body m-b-1">
+                    <div data-testid="home-title" className="card-title h5 mtb-1">
+                      {home.title}
+                    </div>
+                    <div data-testid="home-location">
+                      {home.location}
+                    </div>
+                    <div data-testid="home-price">${home.price}/night</div>
+                  </div>
                 </div>
-            )
+              </div>
+            );
         });
 
     return(
-        <div>
-            <div data-testid="home">
+        <div className="container m4 text-center">
+            <h1>Our Homes</h1>
+            <h4>Choose from our selection of wonderful homes</h4>
+            <div className="row d-flex justify-content-center">
                 {homes}
             </div>
         </div>
--- src/components/home.test.js
+++ src/components/home.test.js
@@ -14,31 +14,31 @@ beforeEach(async () => {
           title: "Test Home 1",
           image: "images/house-img-1.jpg",
           location: "Test Location 1",
-          price: "$13.99",
+          price: "13.99",
         },
         {
           title: "Test Home 2",
           image: "images/house-img-2.jpg",
           location: "Test Location 2",
-          price: "$10.99",
+          price: "10.99",
         },
         {
           title: "Test Home 3",
           image: "images/house-img-3.jpg",
           location: "Test Location 3",
-          price: "$20.99",
+          price: "20.99",
         },
         {
           title: "Test Home 4",
           image: "images/house-img-4.jpg",
           location: "Test Location 4",
-          price: "$16.99",
+          price: "16.99",
         },
         {
           title: "Test Home 5",
           image: "images/house-img-5.jpg",
           location: "Test Location 5",
-          price: "$19.99",
+          price: "19.99",
         },
       ]);
     });
@@ -79,5 +79,5 @@ it('should show price of home', () => {
     const homePrices = getAllByTestId(container, 'home-price');
 
     expect(homePrices[0]).toBeTruthy();
-    expect(getNodeText(homePrices[0])).toBe("$13.99");
+    expect(getNodeText(homePrices[0])).toBe("$13.99/night");
 });
